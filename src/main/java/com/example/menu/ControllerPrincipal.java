651f9f876a7ac278940c06b293f1b1ba35e8b9e2
javiergmun <javiergmun@gmail.com>
2022-02-14 11:29:01 +0100

    Animación menu

--- src/main/java/com/example/menu/ControllerPrincipal.java
+++ src/main/java/com/example/menu/ControllerPrincipal.java
@@ -1,30 +1,42 @@
 package com.example.menu;
 
-import javafx.event.ActionEvent;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
-import javafx.stage.Stage;
-
-import java.io.IOException;
+import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
 
 public class ControllerPrincipal {
     @FXML
     public Button vista1;
+    @FXML
     public Button vista2;
+    @FXML
     public Button vista3;
+    @FXML
     public Button salir;
-    public Button Menu;
-    public HBox hbox1;
+    @FXML
+    public Button botonMenu;
+    @FXML
     public VBox vbox1;
+    @FXML
     public StackPane stackpane1;
+    @FXML
     public ImageView imagenLogo;
+    @FXML
     public ImageView photo;
+    @FXML
+    public Rectangle rectanguloLimiteDcha;
+    @FXML
+    public Rectangle rectanguloLimiteIzda;
+    private Animation movimiento1;
+    @FXML
+    private ControllerCargarDetalles controllerCargarDetalles;
 
     @FXML
     public void vista1(){
@@ -40,21 +52,35 @@ public class ControllerPrincipal {
     }
     @FXML
     public void salir(){
-        Menu.setTranslateX(0);
         photo.setImage(new Image("https://www.seekpng.com/png/small/87-872219_lupa-icon.png"));
-        imagenLogo.setVisible(true);
-        vbox1.setTranslateX(-75);
+        imagenLogo.setTranslateY(0);
+        botonMenu.setTranslateY(0);
+        vbox1.setTranslateX(0);
         stackpane1.setTranslateX(0);
-        //hbox1.setVisible(true);
-
     }
+
+    //ANIMACIÓN DISEÑADA PARA CUALQUIER BOTON, SOLO CAMBIAR EL TRANSLATE Y EL LIMITE CON EL QUE CHOCA :)
     @FXML
     public void menu(){
-        Menu.setTranslateX(100);
-        vbox1.setTranslateX(75);
-        stackpane1.setTranslateX(20);
-        imagenLogo.setVisible(false);
-        //hbox1.setVisible(false);
+        this.movimiento1=new Timeline((new KeyFrame(Duration.millis(17), lambda->{
+            iniciarMovimientoMenu();
+            pararMovimientoMenu();
+        })));
+        movimiento1.setCycleCount(Animation.INDEFINITE);
+        movimiento1.play();
+        //imagenLogo.setVisible(false);
     }
+    public void iniciarMovimientoMenu(){
+        botonMenu.setTranslateY(botonMenu.getTranslateY()-3);
+        vbox1.setTranslateX(vbox1.getTranslateX()+7);
+        stackpane1.setTranslateX(stackpane1.getTranslateX()+3);
+        imagenLogo.setTranslateY(imagenLogo.getTranslateY()+20);
+    }
+    public void pararMovimientoMenu() {
+        if (stackpane1.getBoundsInParent().intersects(rectanguloLimiteDcha.getBoundsInParent())) {
+            movimiento1.stop();
+        }
+    }
+
 
 }
